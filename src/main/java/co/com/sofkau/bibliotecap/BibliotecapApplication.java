f0e49f334ae423e0784eec316bdda39906dcd676
Hollman Rojas <hollmanerojas@gmail.com>
2021-10-13 18:10:23 -0500

    creacion del proyecto

---
+++ src/main/java/co/com/sofkau/bibliotecap/BibliotecapApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.bibliotecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofkau/bibliotecap/BibliotecapApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.bibliotecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
