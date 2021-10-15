982996b5760f4d6a86680dafd1d24032bef424d8
Hollman Rojas <hollmanerojas@gmail.com>
2021-10-15 18:31:38 -0500

    Fix/ route to main run

--- src/main/java/co/com/sofkau/bibliotecap/BibliotecapApplication.java
+++ src/main/java/co/com/sofkau/bibliotecap/BibliotecapApplication.java
@@ -2,10 +2,8 @@ package co.com.sofkau.bibliotecap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("src/main/java/co/com/sofkau")
 public class BibliotecapApplication {
 
 	public static void main(String[] args) {
